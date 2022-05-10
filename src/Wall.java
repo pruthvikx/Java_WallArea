d7391ed7473b829450e7b6b772ffa548c9cba081
Pruthvi Kolhe <65055563+pruthvikx@users.noreply.github.com>
2022-05-10 09:29:41 +0530

    Get wall area using constructors , getters and setters.

---
+++ src/Wall.java
@@ -0,0 +1,50 @@
+public class Wall {
+
+    private double width;
+    private double height;
+
+
+    public Wall() {
+
+    }
+
+    public Wall(double width, double height) {
+        this.width = width;
+        this.height = height;
+
+        if (this.width < 0){
+            this.width = 0;
+        }if (this.height < 0){
+            this.height = 0;
+        }
+
+    }
+
+    public double getWidth() {
+        return this.width;
+    }
+
+    public void setWidth(double width) {
+        if (width < 0){
+            this.width = 0;
+        }else {
+            this.width = width;
+        }
+    }
+
+    public double getHeight() {
+        return this.height;
+    }
+
+    public void setHeight(double height) {
+        if (height < 0){
+            this.height = 0;
+        }else {
+            this.height = height;
+        }
+    }
+
+    public double getArea(){
+        return this.width * this.height;
+    }
+}
